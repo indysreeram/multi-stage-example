fbc00715f0b563ed04d067b6289f51004e96ec69
Sreeram Srinivasaragavan <indysreeram@gmail.com>
2020-04-29 11:20:45 -0400

    first commit

---
+++ src/main/java/com/sreeram/multistageexample/MultistageExampleApplication.java
@@ -0,0 +1,13 @@
+package com.sreeram.multistageexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultistageExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultistageExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sreeram/multistageexample/MultistageExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sreeram.multistageexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultistageExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
